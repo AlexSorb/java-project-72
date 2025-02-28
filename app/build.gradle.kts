c99888833c96590c0cbb384b9d88b3dbeec3697a
Alexandr Ryabinin <zochsa@gmail.com>
2025-02-28 20:58:21 +0300

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,16 +4,24 @@ plugins {
     checkstyle
     jacoco
     id("io.freefair.lombok") version "8.12.2"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
+
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("hexlet.code.App")
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("io.javalin:javalin:6.4.0")
+
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
