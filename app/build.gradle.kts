3ca984b6646a7e6041e5048e2c0b57297968dc07
Alexandr Ryabinin <zochsa@gmail.com>
2025-03-10 15:47:24 +0300

    Add Javalin and logger

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,4 @@
 plugins {
-    java
     application
     checkstyle
     jacoco
@@ -21,6 +20,10 @@ repositories {
 
 dependencies {
     implementation("io.javalin:javalin:6.4.0")
+    implementation("org.slf4j:slf4j-simple:2.0.7")
+    implementation("io.javalin:javalin-rendering:6.1.3")
+    implementation("gg.jte:jte:3.1.9")
+
 
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,20 @@
 package hexlet.code;
 
+import io.javalin.Javalin;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        var app = getApp();
+
+        app.get("/", handler -> handler.result("Hello World"));
+
+        app.start(7070);
+    }
+
+    public static Javalin getApp() {
+        return Javalin.create(javalinConfig -> {
+            javalinConfig.bundledPlugins.enableDevLogging();
+        });
     }
 }
