eae5a55d7cd4417a5f6511e5a58ad1305d4d8963
Alexandr Ryabinin <zochsa@gmail.com>
2025-03-18 00:56:00 +0300

    add getPort function

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,18 +3,27 @@ package hexlet.code;
 import io.javalin.Javalin;
 
 public class App {
+
+    public static final String PORT_NAME = "PORT";
+    public static final String DEFAULT_PORT = "7070";
+
     public static void main(String[] args) {
 
         var app = getApp();
 
         app.get("/", handler -> handler.result("Hello World"));
 
-        app.start(7070);
+        app.start(getPort(PORT_NAME));
     }
 
     public static Javalin getApp() {
         return Javalin.create(javalinConfig -> {
             javalinConfig.bundledPlugins.enableDevLogging();
         });
+    }
+
+    public static int getPort(String portName) {
+        String port = System.getenv().getOrDefault(portName, DEFAULT_PORT);
+        return Integer.parseInt(port);
     }
 }
