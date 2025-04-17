31c44814577ee633e4bf42591b22cc5a93a64b27
Alexandr Ryabinin <zochsa@gmail.com>
2025-04-17 17:05:32 +0300

    update

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,7 @@ package hexlet.code;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import hexlet.code.Util.Date;
 import hexlet.code.repository.BaseRepository;
 import io.javalin.Javalin;
 
@@ -9,7 +10,15 @@ import gg.jte.ContentType;
 import gg.jte.TemplateEngine;
 import io.javalin.rendering.template.JavalinJte;
 import gg.jte.resolve.ResourceCodeResolver;
+import lombok.extern.slf4j.Slf4j;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
+
+@Slf4j
 public class App {
 
     public static final String PORT_NAME = "PORT";
@@ -18,29 +27,34 @@ public class App {
     public static final String JDBC_URL_NAME = "JDBC_DATABASE_URL";
 
     private static HikariConfig config = new HikariConfig();
-    private static HikariDataSource dataSource;
+    private static HikariDataSource ds;
 
     static {
-        config.setJdbcUrl("");
+        config.setJdbcUrl(Date.getBDName());
+        config.setUsername(Date.getUserName());
+        config.setPassword( Date.getPassword() );
+        config.addDataSourceProperty( "cachePrepStmts" , "true" );
+        config.addDataSourceProperty( "prepStmtCacheSize" , "250" );
+        config.addDataSourceProperty( "prepStmtCacheSqlLimit" , "2048" );
+        ds = new HikariDataSource( config );
     }
 
 
-    public static void main(String[] args) {
 
+    public static void main(String[] args) throws IOException {
         var app = getApp();
 
         app.start(getPort(PORT_NAME));
     }
 
-    public static Javalin getApp() {
 
-        var hikariConfig = new HikariConfig();
+    public static Javalin getApp() throws IOException {
 
-        String dbName = System.getenv().getOrDefault(JDBC_URL_NAME, JDBC_URL_DEFAULT);
-        hikariConfig.setJdbcUrl(dbName);
+        BaseRepository.dataSource = new HikariDataSource(config);
+        var sql = readResourceFile("schema.sql");
 
+        log.info(sql);
 
-        BaseRepository.dataSource = new HikariDataSource(hikariConfig);
 
         var app = Javalin.create(javalinConfig -> {
             javalinConfig.bundledPlugins.enableDevLogging();
@@ -48,7 +62,11 @@ public class App {
         });
 
 
-        app.get("/", handler -> handler.result("Hello World"));
+
+        // Обработчик событий
+        app.get("/", handler -> {
+            handler.render("index.jte");
+        });
         return app;
     }
 
@@ -57,10 +75,23 @@ public class App {
         return Integer.parseInt(port);
     }
 
+    // Создание шаблона jte
     private static TemplateEngine createTemplateEngine() {
         ClassLoader classLoader = App.class.getClassLoader();
         ResourceCodeResolver codeResolver = new ResourceCodeResolver("templates", classLoader);
         TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
         return templateEngine;
+    }
+
+
+    // Загрузка данных из ресурсного файла
+    private static String readResourceFile(String fileName) throws IOException {
+
+        var inputStream = App.class.getClassLoader().getResourceAsStream(fileName);
+
+        try (BufferedReader bufferedReader = new BufferedReader(
+                new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+            return bufferedReader.lines().collect(Collectors.joining("\n"));
+        }
     }
 }
---
+++ app/src/main/java/hexlet/code/Util/Date.java
@@ -0,0 +1,18 @@
+package hexlet.code.Util;
+
+public class Date {
+
+    public static String  getBDName() {
+        return System.getenv().getOrDefault("JDBC_DATABASE_URL", "jdbc:h2:mem:project");
+
+    }
+
+    public static String getUserName() {
+        return System.getenv().getOrDefault("user", "jdbc:h2:mem:project");
+    }
+
+    public static String getPassword() {
+        return System.getenv().getOrDefault("password", "jdbc:h2:mem:project");
+    }
+
+}
