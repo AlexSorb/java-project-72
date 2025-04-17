062de6368f87bb6b5ee70aa2ae58a585222c9c5a
Alexandr Ryabinin <zochsa@gmail.com>
2025-04-17 17:23:21 +0300

    update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,6 +23,8 @@ dependencies {
     implementation("io.javalin:javalin-bundle:6.5.0")
     implementation("io.javalin:javalin-rendering:6.5.0")
 
+    implementation("org.postgresql:postgresql:42.7.2")
+
     implementation("org.slf4j:slf4j-simple:2.0.7")
     implementation("com.zaxxer:HikariCP:6.2.1")
     implementation("com.h2database:h2:2.3.232")
