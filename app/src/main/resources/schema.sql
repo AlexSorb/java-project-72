143931451230e8e59221ea482d1f91f8a5e142de
Alexandr Ryabinin <146195602+AlexSorb@users.noreply.github.com>
2025-04-30 11:33:36 +0300

    Update schema.sql

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS urls;
 
 CREATE TABLE urls (
-    id integer PRIMARY KEY AUTO_INCREMENT,
+    id serial PRIMARY KEY,
     name varchar(255) NOT NULL,
     created_at timestamp
 );
