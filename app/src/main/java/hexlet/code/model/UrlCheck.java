9c8a2688a6c8e2da5990889caf49dac94d7de7ca
Alexandr Ryabinin <zochsa@gmail.com>
2026-04-25 14:17:46 +0300

    Added description of the UrlCheck class

--- app/src/main/java/hexlet/code/model/UrlCheck.java
+++ app/src/main/java/hexlet/code/model/UrlCheck.java
@@ -7,6 +7,17 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+/**
+ * Represents a unit of validation for a URL entered on a website.
+ *
+ *<p>
+ *     This class is used to encapsulate the validation of a URL sent through a website interface.
+ *     The encapsulated URL validation can then be stored in the database for later use.
+ *</p>
+ *
+ * @author Alexander Ryabinin
+ * @version 1.0
+ */
 @Getter
 @Setter
 @AllArgsConstructor
@@ -20,6 +31,14 @@ public class UrlCheck {
     private Long urlId;
     private LocalDateTime createdAt;
 
+    /**
+     *
+     * @param statusCode
+     * @param title
+     * @param h1
+     * @param description
+     * @param urlId
+     */
     public UrlCheck(int statusCode, String title, String h1, String description, Long urlId) {
         this.statusCode = statusCode;
         this.title = title;
