155e0f5d051d6ce74d3ffe1d5997b0e9c28d4ea4
Alexandr Ryabinin <zochsa@gmail.com>
2026-04-25 15:02:15 +0300

    Added test initialization

--- app/src/test/java/hexlet/code/model/UrlCheckTest.java
+++ app/src/test/java/hexlet/code/model/UrlCheckTest.java
@@ -1,8 +1,35 @@
 package hexlet.code.model;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * The class contains tests of the UrlCheck class.
  */
 
 public class UrlCheckTest {
+
+    /**
+     * Check that the UrlCheck constructor does not initialize other fields.
+     */
+    @Test
+    public void initializationUrlCheck() {
+        int testStatusCode = 200;
+        String testTitle = "Test line of the title block";
+        String testH1 = "Test line of the h1 block";
+        String testDescription = "Test description";
+        long testId = 1L;
+
+        var cut = new UrlCheck(testStatusCode, testTitle, testH1, testDescription, testId);
+
+        assertEquals(testStatusCode, cut.getStatusCode());
+        assertEquals(testTitle, cut.getTitle());
+        assertEquals(testH1, cut.getH1());
+        assertEquals(testDescription, cut.getDescription());
+        assertEquals(testId, cut.getUrlId());
+
+        assertNull(cut.getId());
+        assertNull(cut.getCreatedAt());
+    }
 }
