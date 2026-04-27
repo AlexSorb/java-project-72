ea966a64e1f7b92a9b4f37116fd9fb2cf0974491
Alexandr Ryabinin <zochsa@gmail.com>
2026-04-27 12:38:40 +0300

    Added description of the BaseRepository class

--- app/src/main/java/hexlet/code/repository/BaseRepository.java
+++ app/src/main/java/hexlet/code/repository/BaseRepository.java
@@ -5,6 +5,14 @@ import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
 
+/**
+ * Represents the base class of repositories.
+ *
+ * <p>
+ * This class is a simple base class from which all repositories inherit. It stores a static
+ * field with a database connection.
+ * </p>
+ */
 public class BaseRepository {
     public static HikariDataSource dataSource;
 
