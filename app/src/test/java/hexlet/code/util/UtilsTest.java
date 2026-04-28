675dc0cb3db9cf59c7c7554c8030049d30492314
Alexandr Ryabinin <zochsa@gmail.com>
2026-04-28 16:44:15 +0300

    Update tests

--- app/src/test/java/hexlet/code/util/UtilsTest.java
+++ app/src/test/java/hexlet/code/util/UtilsTest.java
@@ -7,25 +7,20 @@ import java.net.MalformedURLException;
 import java.net.URISyntaxException;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class UtilsTest {
-    private final String testString = "Test text";
+    private static final String TEST_STRING = "Test text";
 
-    private final String fullUrlWithoutPort = "https://example.com/api/v1/users?status=active&sort=desc";
-    private final String shortUrlWithoutPort = "https://example.com/";
+    private static final String FULL_URL_WITHOUT_PORT = "https://example.com/api/v1/users?status=active&sort=desc";
+    private static final String SHORT_URL_WITHOUT_PORT = "https://example.com/";
 
+    private static final String FULL_URL_WITH_PORT = "http://localhost:3000/auth/register";
+    private static final String SHORT_URL_WITH_PORT = "http://localhost:3000";
 
-    private final String fullUrlWithPort = "http://localhost:3000/auth/register";
-    private final String shortUrlWithPort = "http://localhost:3000";
+    private static final String RIGHT_URL_WITHOUT_PORT = "https://example.com";
+    private static final String RIGHT_URL_WITH_PORT = "http://localhost:3000";
 
-    private final String rightUrlWithoutPort = "https://example.com";
-    private final String rightUrlWithPort = "http://localhost:3000";
-
-
-    @Test
-    public void getNormalizeUrlTest() {
-        // TO DO
-    }
 
     @Test
     public void inputNullAsUrlTest() {
@@ -41,42 +36,38 @@ public class UtilsTest {
 
     @Test
     public void inputCorrectUrlWithoutPortTest() throws MalformedURLException, URISyntaxException {
-        var resultFullUrlWithoutPort = Utils.getNormalizeUrl(fullUrlWithoutPort);
-        assertEquals(rightUrlWithoutPort, resultFullUrlWithoutPort);
+        var resultFullUrlWithoutPort = Utils.getNormalizeUrl(FULL_URL_WITHOUT_PORT);
+        assertEquals(RIGHT_URL_WITHOUT_PORT, resultFullUrlWithoutPort);
 
-        var resultShortUrlWithoutPort = Utils.getNormalizeUrl(shortUrlWithoutPort);
-        assertEquals(rightUrlWithoutPort, resultShortUrlWithoutPort);
+        var resultShortUrlWithoutPort = Utils.getNormalizeUrl(SHORT_URL_WITHOUT_PORT);
+        assertEquals(RIGHT_URL_WITHOUT_PORT, resultShortUrlWithoutPort);
     }
 
     @Test
     public void inputCorrectUrlWithPortTest() throws MalformedURLException, URISyntaxException {
-        var resultFullUrlWithPort = Utils.getNormalizeUrl(fullUrlWithPort);
-        assertEquals(rightUrlWithPort, resultFullUrlWithPort);
+        var resultFullUrlWithPort = Utils.getNormalizeUrl(FULL_URL_WITH_PORT);
+        assertEquals(RIGHT_URL_WITH_PORT, resultFullUrlWithPort);
 
-        var resultShortUrlWithPort = Utils.getNormalizeUrl(shortUrlWithPort);
-        assertEquals(rightUrlWithPort, resultShortUrlWithPort);
+        var resultShortUrlWithPort = Utils.getNormalizeUrl(SHORT_URL_WITH_PORT);
+        assertEquals(RIGHT_URL_WITH_PORT, resultShortUrlWithPort);
     }
 
     @Test
     public void getDescriptionTest() {
-        String body = "<meta name=\"description\" content=\"" + testString + "\">";
+        String body = "<meta name=\"description\" content=\"" + TEST_STRING + "\">";
         var getTeg = Utils.getDescription(body);
-        assertEquals(getTeg, testString);
+        assertEquals(TEST_STRING, getTeg);
 
         String wrongBody = "<meta name=\"anotherName\" content=\"should not match\">";
         var resultAnotherName = Utils.getDescription(wrongBody);
-        assertEquals(resultAnotherName, "");
+        assertTrue(resultAnotherName.isEmpty());
 
         String emptyBody = "";
         var resultEmptyBody = Utils.getDescription(emptyBody);
-        assertEquals(resultEmptyBody, "");
+        assertTrue(resultEmptyBody.isEmpty());
 
         var resultNullString = Utils.getDescription(null);
-        assertEquals(resultNullString, "");
+        assertTrue(resultNullString.isEmpty());
     }
 
-    @Test
-    public void getDescriptionTest2() {
-
-    }
 }
