f41b0e7958d2bf4c7b1212ad073d5398167c6f7c
Alexandr Ryabinin <zochsa@gmail.com>
2026-04-28 16:22:18 +0300

    Added description of the BasePage class

--- app/src/main/java/hexlet/code/dto/BasePage.java
+++ app/src/main/java/hexlet/code/dto/BasePage.java
@@ -2,7 +2,14 @@ package hexlet.code.dto;
 
 import lombok.Getter;
 import lombok.Setter;
-
+/**
+ * This is a DTO class for passing data to a web page.
+ *
+ * <p>
+ This class is the base class for all DTO classes representing page strobes for passing
+ to website pages.
+ * </p>
+ */
 @Getter
 @Setter
 public class BasePage {
