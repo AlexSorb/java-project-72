59152bc4fb92a5051583fc3f88ebb1789a034ab1
Alexandr Ryabinin <zochsa@gmail.com>
2026-04-30 18:03:59 +0300

    Added dependency all function

--- app/src/main/java/hexlet/code/util/NamedRoutes.java
+++ app/src/main/java/hexlet/code/util/NamedRoutes.java
@@ -7,21 +7,43 @@ package hexlet.code.util;
  * @version 1.0
  */
 public class NamedRoutes {
-
+    /**
+     * Used to get a link to the home page of a website.
+     * @return The root path of the website as <code>String</code>.
+     */
     public static String indexPath() {
         return "/";
     }
+
+    /**
+     * Used to get a link to the main page with a list of added URLs.
+     * @return The path to the page with the added URL as <code>String</code>
+     */
     public static String urlsPath() {
         return "/urls";
     }
+
+    /**
+     * Used to get a link to a page with a specific URL by the specified ID.
+     * @param id ID of the required URL in the database
+     * @return The path to the page with the added URL as <code>String</code>.
+     */
     public static String urlsIdPath(Long id) {
         return "/urls/" + id;
     }
 
+    /**
+     * Used to get a path with a prepared template for adding id at runtime.
+     * @return The path with the id addition pattern as <code>String</code>.
+     */
     public static String urlsIdPath() {
         return "/urls/{id}";
     }
 
+    /**
+     * Used to get the path to check a specific URL.
+     * @return The path to the check as <code>String</code>.
+     */
     public static String urlIdChecksPath() {
         return "/urls/{id}/checks";
     }
